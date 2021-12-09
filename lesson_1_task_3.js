94a4206ff8bc91dd58c0725fdf579764b5ef5903
KolesnikIvan <86139761+KolesnikIvan@users.noreply.github.com>
2021-12-09 19:34:36 +0500

    Add files via upload
    ДЗ от первого урока про основы js.

---
+++ lesson_1_task_3.js
@@ -0,0 +1,10 @@
+/*3. Объявить две переменные: admin и name.
+ * Записать в name строку "Василий". 
+ * Скопировать значение из name в admin. 
+ * Вывести admin(должно вывести «Василий»).*/
+// JavaScript source code
+
+var name = 'Василий'
+var admin
+admin = name
+console.log(admin)
